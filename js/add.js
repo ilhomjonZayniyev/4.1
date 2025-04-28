bba40be13a74bf2416989cedd936e117a8e9abe5
ilhomjonZayniyev <ilhomjonzayniyev7@gmail.com>
2025-04-29 00:10:03 +0500

    sdsdgsd

---
+++ js/add.js
@@ -0,0 +1,55 @@
+// {
+//     function kop(a){
+//         return a*2
+//     }
+//     console.log(kop(5));
+    
+// }
+// {
+//    function ism(ism){
+//     return`Salom ${ism}`
+//    }
+//    console.log(ism("Aziza"));
+   
+// }
+// {
+//     function musbat(n){
+//         return n>0?"musbat":n<0?"manfiy":"nol"
+//     }
+//     console.log(musbat(1));
+// }
+// {
+//     function juftToq(n){
+//         return n/2==0?"juft":"toq"
+//     }
+//     console.log(juftToq(13));
+    
+// }
+// {
+//     function kattaKichik (a,b){
+//         return a<b?b:a
+//     }
+//     console.log(kattaKichik(3,45));
+    
+// }
+// !!!!!!!!! {
+//    function chiqaruvchi (a){
+    
+// let a=""
+// while(i<=b){
+//     a=i
+//   i++
+// }
+// return a
+//    }
+//    console.log(chiqaruvchi(7));
+   
+// }
+function array([]){
+    let sum=0
+    for (i=0; i<array([]).length;i++){
+        sum+=array([i])
+    }
+    return sum
+}
+console.log(array([1,2,3,4,45]));
